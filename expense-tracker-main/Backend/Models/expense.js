8321d3c59ccdfff6b26843fa656d10d467ff93ea
Anuj Kumar <anujkumar0799@gmail.com>
2023-12-24 23:55:38 +0530

    Daily Expense features added

--- expense-tracker-main/Backend/Models/expense.js
+++ expense-tracker-main/Backend/Models/expense.js
@@ -1,25 +1,28 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
-const Expense = sequelize.define('expense',{
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement:true,
-        allowNull:false,
-        primaryKey:true
+const Expense = sequelize.define('expense', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
     },
-    Name:{
-        type:Sequelize.STRING,
-        allowNull:false
+
+    Name: {
+        type: Sequelize.STRING,
+        allowNull: false
     },
-    Desc:{
-        type:Sequelize.STRING,
-        allowNull:false
+
+    Desc: {
+        type: Sequelize.STRING,
+        allowNull: false
     },
-    Price:{
-        type:Sequelize.INTEGER,
-        allowNull:false
+    Price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
     }
+
 });
 
 module.exports = Expense;
